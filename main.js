d6fb46a1e55a0f69b04a782eb459168bcb8eb101
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-07-06 11:57:10 +0300

    practice "for"

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ cars.unshift("Toyota");
 
 console.log(cars);*/
 
+
+
+
 // object
 
 /*const person = {
@@ -24,8 +27,11 @@ const { name1, age, isStudent, address: {street}} = person; //yangiroq funksiyas
 
 console.log( name1, age, isStudent, street );*/
 
+
+
+
 //JSON biz bunday malumotni backendga jo'natayotganimizda ishlatamiz ya'ni string qilib jo'natiladi
-const person = {
+/*const person = {
     name1: "Abdullajon",
     age: 14,
     address: {
@@ -34,6 +40,36 @@ const person = {
     }    
 }
 
-console.log( JSON.stringify(person));
+console.log( JSON.stringify(person));*/
 
 //Bunda ma'lumotlarni hammasi jsonga o'tadi va o'ziga xos belgilar oladi ya'ni ":" ni ham hamma veriableslarga qo'yib beradi
+
+
+
+const students = [
+    {
+        fname: "Jane Doe",
+        age: 23,
+        gradiated: false
+    },
+    {
+        fname: "Jane Smith",
+        age: 21,
+        gradiated: true 
+    },
+    {
+        fname: "Bob Mark",
+        age: 33,
+        gradiated: true 
+    }
+];
+
+//for degani biron narsa to'xtamaguncha aylanib kelaveradi
+/*for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}*/
+
+for (let i = 0; i <= students.length; i++) {
+    console.log(i);
+    console.log(students[i] /*.fname qilsak faqat ismni oladi*/);
+}
